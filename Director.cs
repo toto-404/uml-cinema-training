a1a8a3feb32d54365c21ca4f4d120fe4dce6c504
Olutobi <olutobi.akinsinmide@edouardgand.fr>
2026-03-12 16:54:04 +0100

    Exo 2 classe Director

---
+++ Director.cs
@@ -0,0 +1,42 @@
+class Director
+{
+    private string firstName;
+    private string lastName;
+    private string country;
+
+    public string? GetFirstName()
+    {
+        return firstName;
+    }
+    public void SetFirstName( string firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public string GetLastName()
+    {
+        return lastName;
+    }
+
+    public void SetLastName(string lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public string GetCountry()
+    {
+        return country;
+    }
+
+    public void SetCountry( string country)
+    {
+        this.country = country;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Nom réalisateur: {GetLastName()}");
+        Console.WriteLine($"Prenom: {GetFirstName()}");
+        Console.WriteLine($"Pays: {GetCountry()}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,9 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Movie
         Movie film1 = new Movie();
         film1.Title = "The Time Traveler's Paradox";
         film1.Duration = 140;
         film1.Display();
+        Console.WriteLine();
+        //Director
+        Director dir1 = new Director();
+        dir1.SetFirstName("John");
+        dir1.SetLastName("Doe");
+        dir1.SetCountry("Malaysia");
+        dir1.Display();
     }
 }
